2e379b21ee9ec458e14bc4aee5d1b137e7a9e8fa
yogendrapal <yogendra.pal3@gmail.com>
2015-05-09 15:13:38 +0530

    Initial Commit

---
+++ Cpp/first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"; //this is single line comment
+    /*this is
+    multiple line
+    comment */
+    return 0;
+}
